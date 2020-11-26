24b0ba78573802121f2a8a32ed9992e16974d145
DonsetPG <paul.garnier.97@gmail.com>
2020-11-26 19:02:19 +0100

    Exo 1 ok, quoi que peut repasser en *float, exo 2 a voir

--- Exercice1.cpp
+++ Exercice1.cpp
@@ -1,16 +1,27 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("inline")
+//#pragma GCC optimize("omit-frame-pointer")
+//#pragma GCC optimize("unroll-loops")
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
+float norm(vector<float> U, int n){
+    // Must compute the sum of root of the absolute value of each component.
+    float ans = 0;
+    for (int i = 0 ; i < n ; i++){
+        ans += sqrt(abs(U[i]));
+    }
+    return ans;
+};
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    vector<float> vec {1.0, 1.0, 1.0};
+    float ans = norm(vec,3);
+    cout << ans  << endl;
 }
